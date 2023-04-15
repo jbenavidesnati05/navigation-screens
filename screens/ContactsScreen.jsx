70720412413a17f1eb25e3876a67f24e65cf054a
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-04-15 15:21:13 -0500

    ok

--- screens/ContactsScreen.jsx
+++ screens/ContactsScreen.jsx
@@ -1,9 +1,12 @@
 import { Text, View, Button } from 'react-native';
 import {styles} from '../assets/styles/styles'
 
-function Contacts(){
+function Contacts({route}){
+  const {email, phone} = route.params
     return(
       <View style={styles.container}>
+      <Text>Telefono:{phone}</Text>
+      <Text>Correo:{email}</Text>
         <Text style={{fontWeight:'bold'}}>Estamos en Contactenos</Text>
       </View>
     );
--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -2,9 +2,21 @@ import { Text, View, Button } from 'react-native';
 import {styles} from '../assets/styles/styles'
 
 function ProfileScreen({navigation}){
+
+  let email = 'a@gmail.com';
+  let phone = '3434534'
+  
     return(
       <View style={styles.container}>
         <Text style={{fontWeight:'bold'}}>Estamos en Perfil del Usuario</Text>
+        <Button
+        title='Contactenos'
+        onPress={()=>{
+          navigation.navigate('Contacts',{email:email, phone:phone})
+        }}
+        >
+
+        </Button>
         <Button
           title="Ir a Contactenos"
           onPress={()=>{
